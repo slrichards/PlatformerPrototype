b58e82baede2e2d6c92465bcc88a7e15f7a75c30
saintlula <ehlinazdy@gmail.com>
2025-08-03 18:51:21 +1000

    Enemy player added. Enemy health machanics added, update waiting

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Variables for the enemy controller
+    [Header("Enemy Settings")]
+    [SerializeField] private GameObject Player;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float enemyHealth = 3f;
+
+    private float distance;
     void Start()
     {
         
@@ -11,6 +17,20 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Takes in 2 transforms, finds the distance between them, and moves the enemy towards the player.
+        //Built into C#
+        distance = Vector2.Distance(transform.position, Player.transform.position);
+        Vector2 direction = Player.transform.position - transform.position;
+
+        transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, speed * Time.deltaTime);
+
+    }
+    public void TakeDamage(float damage)
+    {
+        enemyHealth -= damage;
+        if (enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
